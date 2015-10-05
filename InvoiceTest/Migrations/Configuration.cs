0d16f88d59967be3d37992e7dc62dd15de691697
Ken <ken.ho170381@gmail.com>
2015-10-05 12:12:41 +0700

    Invoice Test

---
+++ InvoiceTest/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+using InvoiceTest.Models;
+
+namespace InvoiceTest.Migrations
+{
+    using InvoiceTest.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Identity.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            if (!context.Users.Any(u => u.UserName == "sallen"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "sallen" };
+
+                manager.Create(user, "password");
+            }
+        }
+    }
+}
