bc9e09e6a6c55041a532d9edc089969c80744cee
Ken <ken.ho170381@gmail.com>
2015-10-08 13:31:28 +0700

    remove page not show

--- InvoiceTest/Controllers/HomeController.cs
+++ InvoiceTest/Controllers/HomeController.cs
@@ -9,23 +9,9 @@ namespace InvoiceTest.Controllers
 
     public class HomeController : Controller
     {
-            [Authorize]
+        [Authorize]
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
